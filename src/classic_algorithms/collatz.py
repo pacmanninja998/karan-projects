b02ebef7b64b7ad48db8a4d859c65fa403e0e0bc
pacmanninja998 <pacmanninja998@gmail.com>
2025-01-08 10:11:20 -0500

    Update collatz.py
    if n <= 1  is redundant in code as the internal while loop checks if n isn't equal to 1.

--- src/classic_algorithms/collatz.py
+++ src/classic_algorithms/collatz.py
@@ -1,15 +1,12 @@
 def collatz(n):
 	count = 0
-	if n <= 1:
-		return count
-	else:
-		while (n != 1):
-			if n % 2 == 0:
-				n = n / 2
-				count += 1
-			else:
-				n = (n * 3) + 1
-				count += 1
+	while (n != 1):
+		if n % 2 == 0:
+			n = n / 2
+			count += 1
+		else:
+			n = (n * 3) + 1
+			count += 1
 	return count
 
 # Test the first 100 numbers
